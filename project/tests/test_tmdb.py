d2f57b475713c91bc657f86c8bb645a8d431136f
H <achraf.ghb@gmail.com>
2019-10-04 15:39:47 +0200

    Typo in comment fix

--- project/tests/test_tmdb.py
+++ project/tests/test_tmdb.py
@@ -10,12 +10,15 @@ from project.tests.base import BaseTestCase
 
 
 class TestTmdb(BaseTestCase):
-
+    """
+    Testing if we have the good responses from the api
+    """
     def test_discover(self):
         response = Tmdb.discover()
         self.assertTrue(int(response.status_code) == 200)
         data = json.loads(response.data.decode())
         self.assertTrue(isinstance(data['results'], list))
+        # TODO check if all the shows are in the good format
 
 
 if __name__ == '__main__':
